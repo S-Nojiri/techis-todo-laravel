23b4b78651c240b02993bb0521261462feb66202
S-Nojiri <the.ace.of.spades.ars@gmail.com>
2022-01-17 15:45:52 +0900

    change migration

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -15,9 +15,9 @@ class CreatePersonalAccessTokensTable extends Migration
     {
         Schema::create('personal_access_tokens', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable')->charset("utf8_unicode_ci");
+            $table->morphs('tokenable')->charset("utf8")->unique();
             $table->string('name');
-            $table->string('token', 64)->charset("utf8_unicode_ci")->unique();
+            $table->string('token', 64)->charset("utf8")->unique();
             $table->text('abilities')->nullable();
             $table->timestamp('last_used_at')->nullable();
             $table->timestamps();
